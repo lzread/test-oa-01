ba546300ea75df1125a22c14a6bcdf555fdcacef
lzread <lzread@163.com>
2021-01-28 21:05:05 +0800

    add

--- src/api/menu.js
+++ src/api/menu.js
@@ -6,3 +6,39 @@ export function getMenus() {
   });
 }
 
+export function list() {
+  return request({
+    url: "/menu/getAllMenus",
+    method: "get"
+  });
+}
+
+export function listSmall() {
+  return request({
+    url: "/menu/getMiniMenus",
+    method: "get"
+  });
+}
+
+export function add(data) {
+  return request({
+    url: "/menu/add",
+    method: "post",
+    data
+  });
+}
+
+export function edit(data) {
+  return request({
+    url: "/menu/edit",
+    method: "post",
+    data
+  });
+}
+
+export function del(id) {
+  return request({
+    url: `/menu/del/${id}`,
+    method: "post",
+  });
+}
--- src/views/system/menus/index.vue
+++ src/views/system/menus/index.vue
@@ -1,9 +1,199 @@
 <template>
   <div>
+    <el-button @click="addHandle"></el-button>
+    <el-table
+      :data="rows"
+      row-key="id"
+      default-expand-all
+      :tree-props="{children:'children'}"
+    >
+      <el-table-column
+        prop="title"
+        label="标题"
+      ></el-table-column>
+      <el-table-column label="类别">
+        <template slot-scope="scope">
+          {{scope.row.type == 0 ? '菜单' : '权限按钮'}}
+        </template>
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            @click="addChildrenHandle(scope.row)"
+            type="text"
+            v-if="scope.row.type == 0"
+          >添加下级菜单</el-button>
+          <el-button
+            @click="addPermissionHandle(scope.row)"
+            type="text"
+            v-if="scope.row.type == 0"
+          >添加权限菜单</el-button>
+          <el-button
+            @click="editHandle(scope.row)"
+            type="text"
+            v-if="scope.row.type == 0"
+          >编辑菜单</el-button>
+          <el-button
+            @click="editPermissionHandle(scope.row)"
+            type="text"
+            v-if="scope.row.type == 1"
+          >编辑按钮</el-button>
+          <el-button
+            @click="deleteHandle(scope.row)"
+            type="text"
+          >删除</el-button>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+    <el-dialog
+      width="600px"
+      :visible.sync="dialogVisible"
+      :title="dialogVisibleType == 'add' ? '新建菜单' : '编辑菜单'"
+    >
+      <el-form
+        ref="menu"
+        :model="items"
+        label-width="100px"
+        label-position="right"
+      >
+        <el-form-item label="菜单名称">
+          <el-input
+            v-model="items.title"
+            placeholder="请输入"
+          />
+        </el-form-item>
+
+        <el-form-item label="父级菜单">
+
+          <treeselect
+            v-model="items.parent_id"
+            :default-expand-level="1"
+            :searchable="false"
+            :clearable="false"
+            :options="treeSelectData"
+            :normalizer="normalizer"
+            :instance-id="items.id || 0"
+            placeholder="请选择"
+          />
+
+        </el-form-item>
+
+        <el-form-item label="路由名称">
+          <el-input
+            v-model="items.name"
+            placeholder="请输入"
+          />
+        </el-form-item>
+
+        <el-form-item label="组件路径">
+          <el-input
+            v-model="items.component"
+            placeholder="请输入"
+          />
+        </el-form-item>
+
+        <el-form-item label="菜单路径">
+          <el-input
+            v-model="items.path"
+            placeholder="请输入"
+          />
+        </el-form-item>
+
+      </el-form>
+
+      <div slot="footer">
+        <el-button
+          type="danger"
+          @click="reset('menu')"
+        >取消</el-button>
+        <el-button
+          type="primary"
+          @click="commitHandle()"
+        >提交</el-button>
+      </div>
+    </el-dialog>
 
   </div>
 </template>
 
 <script>
+// import the component
+import Treeselect from "@riophae/vue-treeselect";
+// import the styles
+import "@riophae/vue-treeselect/dist/vue-treeselect.css";
+import { list, listSmall, add, edit, del } from "@/api/menu";
+export default {
+  name: "menus",
+  data() {
+    return {
+      rows: [],
+      items: {},
+      parentItems: [],
+      dialogVisible: false,
+      dialogVisibleType: "",
+    };
+  },
+  computed: {
+    treeSelectData() {
+      return [{
+        id: 0,
+        parent_id: -1,
+        title: "根节点",
+        children: this.parentItems,
+      }];
+    },
+  },
+  created() {
+    this.getList();
+    this.getParentMap();
+  },
+  components: { Treeselect },
+  methods: {
+    normalizer(node, instanceId) {
+      let disabled = false;
+      if (node.id == instanceId && node.parent_id != -1) {
+        disabled = true;
+      }
+      return {
+        id: node.id,
+        label: node.title,
+        isDisabled: disabled,
+        children: node.children,
+      };
+    },
+    async getParentMap() {
+      const { data } = await listSmall();
+      this.parentItems = data;
+    },
+    async getList() {
+      const { data } = await list();
+      this.rows = data;
+    },
+    addHandle() {
+      this.dialogVisible = true;
+      this.dialogVisibleType = "add";
+      this.items = {};
+    },
+    editHandle(row) {
+      this.dialogVisible = true;
+      this.dialogVisibleType = "edit";
+      this.items = row;
+    },
+    async commitHandle() {
+      if (this.dialogVisibleType == "add") {
+        await add(this.items);
+      } else {
+        await edit(this.items);
+      }
+    },
 
+    addChildrenHandle() {},
+    addPermissionHandle() {},
+
+    editPermissionHandle() {},
+    deleteHandle() {},
+  },
+};
 </script>

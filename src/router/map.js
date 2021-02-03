d3a0dbe9556dbf02a11f4a313493f981dfd3237a
lzread <lzread@163.com>
2021-02-03 09:29:32 +0800

    fix 2021-02-02 修复因为没有设置索引导致的没有显示部门

--- src/router/map.js
+++ src/router/map.js
@@ -3,6 +3,7 @@ const routerComponentsMap = {
     "users": () => import('@/views/system/users/index'),
     "menus": () => import('@/views/system/menus/index'),
     "roles": () => import('@/views/system/roles/index'),
+    "department": () => import('@/views/system/department/index'),
     "work": () => import('@/components/Layout'),
     "notice": () => import('@/views/work/notice/index')
 
